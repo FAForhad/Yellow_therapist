97f0e5ba54a80f53d5e190b3530fe2e32d3036df
FAForhad <faforhad450@gmail.com>
2023-03-25 17:26:40 +0600

    feature1_sectionPart1_create

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navber from "./Components/Navber/Navber";
 import Banner from "./Components/Banner/Banner";
+import Feature1 from "./Components/Features/Feature1/Feature1";
 
 function App() {
   return (
     <div className="App">
       <Navber></Navber>
       <Banner></Banner>
+      <Feature1></Feature1>
     </div>
   );
 }
---
+++ src/Components/Features/Feature1/Feature1.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../Feature.css";
+import { GoLocation } from "react-icons/go";
+import { BsMailbox2 } from "react-icons/bs";
+import { TbPhoneCall } from "react-icons/tb";
+
+const Feature1 = () => {
+  return (
+    <div>
+      <div className="grid lg:grid-cols-3 md:grid-cols-1 bg">
+        <div className="bg-yellow-100 opacity-70  py-12  px-8 text-center lg:border-r-2 border-x-gray-500">
+          <div className="my-6">
+            <h1 className="px-4 text-2xl uppercase">OUR </h1>
+            <h1 className="px-4 text-4xl uppercase font-bold">CONTACTS</h1>
+          </div>
+          <h2 className="px-4 lg:mx-14 py-2 border-b-2 border-black flex items-center gap-2 justify-center">
+            <GoLocation></GoLocation>
+            264, Chorsolakia, Kishoreganj
+          </h2>
+          <h2 className="px-4 lg:mx-14 my-4 py-2 border-b-2 border-black flex items-center gap-2 justify-center">
+            <TbPhoneCall></TbPhoneCall>
+            To call directly +8801845769869
+          </h2>
+          <h2 className="px-4 lg:mx-14 my-4 py-2 border-b-2 border-black flex items-center gap-2 justify-center">
+            <BsMailbox2></BsMailbox2>
+            contact@theyellowtherapist.com
+            <br />
+            operations@theyellowtherapist.com
+            <br />
+            support@theyellowtherapist.com
+            <br />
+            legal@theyellowtherapist.com
+          </h2>
+        </div>
+        <div className="bg-yellow-100  opacity-60 py-12  px-8 text-center lg:border-r-2 border-x-gray-500">
+          <div className="my-6">
+            <h1 className="px-4 text-2xl uppercase ">MAKE AN</h1>
+            <h1 className="px-4 text-4xl uppercase font-bold">APPOINTMENT</h1>
+          </div>
+        </div>
+        <div className="bg-yellow-100  opacity-50 py-12  px-8 text-center">
+          <div className="my-6">
+            <h1 className="px-4 text-2xl uppercase ">OPENING</h1>
+            <h1 className="px-4 text-4xl uppercase font-bold">HOURS</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feature1;
