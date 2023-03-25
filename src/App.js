dd91e1000b51cf4a5a5231a8f8c3b6e9aac2f426
FAForhad <faforhad450@gmail.com>
2023-03-25 20:08:54 +0600

    feature2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navber from "./Components/Navber/Navber";
 import Banner from "./Components/Banner/Banner";
 import Feature1 from "./Components/Features/Feature1/Feature1";
 import Footer from "./Components/Footer/Footer";
+import Feature2 from "./Components/Features/Feature2/Feature2";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navber></Navber>
       <Banner></Banner>
       <Feature1></Feature1>
+      <Feature2></Feature2>
       <Footer></Footer>
     </div>
   );
